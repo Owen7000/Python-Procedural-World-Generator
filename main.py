45bc8ffd81e53bf8b29554e35b7d4cc85da418ca
Owen <77976026+Owen7000@users.noreply.github.com>
2026-04-09 23:02:37 +0100

    Fill map with empty values

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-width, height = 50, 50
+from random import random
+
+width, height = 50,50
 world = [[0 for _ in range(width)] for _ in range(height)]
+
+for y in range(height):
+    for x in range(width):
+        world[y][x] = random()
