7c6a98db9475c345420930d5b53385454fe73e89
Owen <77976026+Owen7000@users.noreply.github.com>
2026-04-09 23:16:14 +0100

    Add colours to the terminal output using ANSI colour codes

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import random
 from noise import pnoise2
+from os import system
 
-width, height = 50, 50
+system("cls") # All that is needed for windows to allow ANSI codes. I still think it's stupid
+
+width, height = 100, 100
 world = [[0 for _ in range(width)] for _ in range(height)]
 
 scale = 20.0
@@ -13,11 +16,11 @@ for y in range(height):
         
 def get_tile_type(value):
     if value < 0.3:
-        return "W"
+        return "\033[44m \033[0m" # Water
     elif value < 0.6:
-        return "G"
+        return "\033[42m \033[0m" # Grass
     else:
-        return "M"
+        return "\033[107m \033[0m" # Mountain
     
 for row in world:
     print("".join(get_tile_type(v) for v in row))
